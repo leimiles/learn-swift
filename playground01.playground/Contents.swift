fb56d4e4d728ecdb669105c64bac93cf9fb7fa1b
Miles <>
2023-11-30 01:07:07 +0800

    <feature> auto type, manual type, shift ops

--- playground01.playground/Contents.swift
+++ playground01.playground/Contents.swift
@@ -1,1 +1,14 @@
-print("hello world")
+import SwiftUI
+
+// shift ops by 2
+print(2 << 2)
+
+// constant, auto type Int
+let number = 10
+
+// constant, auto type double
+let number2 = 3.2
+
+// constant, manually type float
+let number3: Float = 3.2
+print(number3)
